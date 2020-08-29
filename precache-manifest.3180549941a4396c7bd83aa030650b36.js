a03098587dd2ebd1eda9280913527b915a9d4363
dejobratic <dejobratic@gmail.com>
2020-08-29 12:07:37 +0200

    Updates

---
+++ precache-manifest.3180549941a4396c7bd83aa030650b36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0acacc447a1b6869ac251f8ded37266b",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "63c969db03017baecb3b",
+    "url": "/expense-tracker/static/css/main.ed433650.chunk.css"
+  },
+  {
+    "revision": "08f43919e9972d8b6bf6",
+    "url": "/expense-tracker/static/js/2.1eb66d05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker/static/js/2.1eb66d05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63c969db03017baecb3b",
+    "url": "/expense-tracker/static/js/main.0fac3b9c.chunk.js"
+  },
+  {
+    "revision": "7a228fdbfe8715a85308",
+    "url": "/expense-tracker/static/js/runtime-main.91d22b83.js"
+  },
+  {
+    "revision": "74f71ff31a38412c85240ef7cf595a82",
+    "url": "/expense-tracker/static/media/budget-background.74f71ff3.png"
+  }
+]);
